86b85a6e5573c452325a3787d4d9148f32090b79
Awais <Awaisakhtar16@yahoo.com>
2015-12-15 15:51:16 +0100

    ble scanned!

--- app/src/main/java/fotaxis/dpp_android/MainActivity.java
+++ app/src/main/java/fotaxis/dpp_android/MainActivity.java
@@ -1,13 +1,60 @@
 package fotaxis.dpp_android;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothManager bluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
     private DppGraphs dppGraph;
+
+    private static int REQUEST_ENABLE_BT=1001;
+    private static int REQUEST_PERMISSION_LOCATION=1002;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+
+    }
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            debug(device.getName()+","+rssi);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +65,42 @@ public class MainActivity extends AppCompatActivity {
         dppGraph.setView(findViewById(android.R.id.content));
         dppGraph.setXleroLineChart();
 
-        dppGraph.feedMultiple(this);
+        requestLocationPermission();
 
+        //dppGraph.feedMultiple(this);
+        bluetoothManager=(BluetoothManager) getSystemService(this.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        mHandler= new Handler();
+
+
+    }
+    void debug(String log){
+        Log.d("dpp", log);
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        debug(requestCode+""+permissions);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    void requestLocationPermission(){
+        this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_PERMISSION_LOCATION);
+
+        Intent enableLocationIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        this.startActivityForResult(enableLocationIntent, 1003);
+    }
+
+
+
+    void enableBLE(){
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }else{
+            scanLeDevice(true);
+        }
     }
 
     @Override
@@ -37,10 +118,16 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        switch (id){
+            case R.id.action_settings:
 
+                break;
+            case R.id.action_scan:
+                 enableBLE();
+                break;
+            default:
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
